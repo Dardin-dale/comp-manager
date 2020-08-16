c236e5bd558a68a9863bcacca4cdd563b5fad78b
Logan Crecraft <logandalec@gmail.com>
2020-08-15 18:03:05 -0700

    per-env not windows compatible, added plumbing, api routes and testing initiated

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
+require('dotenv-safe').config();
 const express = require("express");
-
 const routes = require("./src/routes");
 const app = express();
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 5000;
 
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
@@ -16,15 +16,10 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 //Create Socket and listen for save book event
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-io.on('connection', (socket) => {
-  socket.on('book saved', function(msg) {
-    //emit book saved event to all users
-    io.emit('book saved', msg);
-  });
-});
-server.listen(PORT);
+// const server = require('http').createServer(app);
+
+
+// server.listen(PORT);
 
 // Start the API server
 app.listen(PORT, () =>
