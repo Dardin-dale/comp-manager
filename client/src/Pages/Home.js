1341a85d599a4c1981c016b35a79dc5eed4aa98c
Logan Crecraft <logandalec@gmail.com>
2020-08-15 18:48:04 -0700

    react-router added

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -8,3 +8,29 @@
 
     Able to navigate to other pages from the nav bar or other UI
 */
+import React from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+
+function Home() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+}
+
+export default Home;
